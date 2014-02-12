7491bf897bd4e82f2d6c837055e209712cd7e95c
Hugo Roque <hugolnx@gmail.com>
2014-02-12 10:42:10 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkshopInscription::Application.config.secret_key_base = 'ede54addaabffe5ad01e151f601e55f958e2e424a312964c1e8ddadf93f1f5078c3b921b09dc265e3adcb5e182c618bfbe33c340db1c9ef7741f335899afb764'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorkshopInscription::Application.config.session_store :cookie_store, key: '_workshop-inscription_session'
